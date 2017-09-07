1f7b7784d95d441d9cef563a6c267d399f50ca59
samsuite <suitester@gmail.com>
2017-09-07 14:18:18 +0200

    grid movement basically works!

--- Assets/Code/Grid.cs
+++ Assets/Code/Grid.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour {
+public class Grid : Singleton<Grid> {
 
-    const float tile_size = 0.5f;
-    const int grid_width = 28;
-    const int grid_height = 31;
+    public const float tile_size = 0.5f;
+    public const int grid_width = 28;
+    public const int grid_height = 31;
 
 	void Start () {
 		
@@ -19,16 +19,16 @@ public class Grid : MonoBehaviour {
         Debug.DrawLine(new Vector3(tile_size*grid_width*0.5f, tile_size*grid_height*-0.5f, 0f), new Vector3(tile_size*grid_width*0.5f, tile_size*grid_height*0.5f, 0f), Color.red);
         Debug.DrawLine(new Vector3(tile_size*grid_width*0.5f, tile_size*grid_height*0.5f, 0f), new Vector3(tile_size*grid_width*-0.5f, tile_size*grid_height*0.5f, 0f), Color.red);
 		
-        for (int i = 0; i < grid_width; i++) {
-            Vector3 top = new Vector3((tile_size*i)-(tile_size*grid_width*0.5f)+(tile_size/2), tile_size*grid_height*0.5f, 0f);
-            Vector3 bot = new Vector3((tile_size*i)-(tile_size*grid_width*0.5f)+(tile_size/2), tile_size*grid_height*-0.5f, 0f);
+        for (int i = 1; i < grid_width; i++) {
+            Vector3 top = new Vector3((tile_size*i)-(tile_size*grid_width*0.5f), tile_size*grid_height*0.5f, 0f);
+            Vector3 bot = new Vector3((tile_size*i)-(tile_size*grid_width*0.5f), tile_size*grid_height*-0.5f, 0f);
 
             Debug.DrawLine(top, bot, Color.blue);
         }
 
-        for (int j = 0; j < grid_height; j++) {
-            Vector3 left = new Vector3(tile_size*grid_width*-0.5f, (tile_size*j)-(tile_size*grid_height*0.5f)+(tile_size/2), 0f);
-            Vector3 right = new Vector3(tile_size*grid_width*0.5f, (tile_size*j)-(tile_size*grid_height*0.5f)+(tile_size/2), 0f);
+        for (int j = 1; j < grid_height; j++) {
+            Vector3 left = new Vector3(tile_size*grid_width*-0.5f, (tile_size*j)-(tile_size*grid_height*0.5f), 0f);
+            Vector3 right = new Vector3(tile_size*grid_width*0.5f, (tile_size*j)-(tile_size*grid_height*0.5f), 0f);
 
             Debug.DrawLine(left, right, Color.blue);
         }
