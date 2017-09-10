f0c7b269a14427311fec85000bac55a704ee6e40
samsuite <suitester@gmail.com>
2017-09-10 17:32:34 -0400

    pacman sprites, changed wall color

--- Assets/Code/PacmanController.cs
+++ Assets/Code/PacmanController.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 public class PacmanController : MonoBehaviour {
 
     PixelPerfectGridMovement movement;
+    public Texture2D[] sprites;
+    Material mat;
+    int current_frame = 0;
+    float animation_timer = 0f;
+    float frame_length = 0.1f;  // in seconds
 
 	void Start () {
+        mat = GetComponent<MeshRenderer>().material;
+
         movement = GetComponent<PixelPerfectGridMovement>();
-		//Pacman always must move
-		movement.current_direction = PixelPerfectGridMovement.Direction.left;
+		movement.current_direction = PixelPerfectGridMovement.Direction.none;
 	}
 	
 	void Update () {
@@ -29,6 +35,32 @@ public class PacmanController : MonoBehaviour {
             movement.desired_direction = PixelPerfectGridMovement.Direction.down;
         }
 
+
+        // only animate while pacman is moving
+        if (movement.current_direction != PixelPerfectGridMovement.Direction.none) {
+            animation_timer += Time.deltaTime;
+            if (animation_timer > frame_length) {
+                animation_timer -= frame_length;
+
+                current_frame = (current_frame+1)%sprites.Length;
+            }
+        }
+
+
+        mat.SetTexture("_MainTex", sprites[current_frame]);
+
+        if (movement.current_direction == PixelPerfectGridMovement.Direction.right) {
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+        else if (movement.current_direction == PixelPerfectGridMovement.Direction.up) {
+            transform.eulerAngles = new Vector3(0f,0f,90f);
+        }
+        else if (movement.current_direction == PixelPerfectGridMovement.Direction.left) {
+            transform.eulerAngles = new Vector3(0f,0f,180f);
+        }
+        else if (movement.current_direction == PixelPerfectGridMovement.Direction.down) {
+            transform.eulerAngles = new Vector3(0f,0f,270f);
+        }
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
